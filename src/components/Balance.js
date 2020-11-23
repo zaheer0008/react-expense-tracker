a83e027750e250a12de31e3bfd84f0fa5e8ba4e8
zaheer0008 <zaheer0008@gmail.com>
2020-11-23 16:14:06 +0500

    Balance added.

---
+++ src/components/Balance.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Balance = ()=>{
+    return(
+        <div>
+            <h4>Current Balance</h4>
+            <h1 id="balance">$0.00</h1>
+        </div>
+    )
+}
+export default Balance
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../App.css'
+
+const Header = () =>{
+    return(
+        <h class='App-header'>
+            React Expense Tracker by Zaheer
+        </h>
+    )
+}
+
+export default Header;
